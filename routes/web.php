8c51c4a826bd222c53834eaf80cdb6b57639f351
Ahmed Abobasha <ahmedabobasah10@gmail.com>
2023-06-09 19:01:29 +0300

    create routes and authantication by auth ui package

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('tracks')->group(function(){
+
+    Route::get('/',function(){
+        return "list of tracks pages";
+    })->name('tracks.index');
+
+    Route::get('/{id}/{name?}',function($id,$name=''){
+
+        return 'Details page of tracks '.$id.' named '.$name ;
+        
+    })->name('track.etails');
+
+    Route::get('/create',function(){})->name('tracks.create');
+
+    Route::Post("/",function(){})->name('tracks.store');
+
+    Route::get('/{id}/edit',function($id){
+
+        return 'this is edit form of tracks'. $id ;
+
+    })->name('tracks.edit');
+
+    Route::put('/{id}',function($id){})->name('tracks.update');
+
+    Route::delete('/{id}',function($id){})->name('tracks.destroy');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
